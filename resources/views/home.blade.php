de4eb8b5b3865bfd1f56f1d5ae5be6a1397faaea
Pablo Julio <pablo.julio.vit@gmail.com>
2021-08-08 13:47:04 -0400

    ADD: Login basico + display dinamico inicial de usuarios

---
+++ resources/views/home.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!DOCTYPE html>
+<html>
+<head>
+    <style>
+        #customers {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #customers td, #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+        #customers tr:hover {background-color: #ddd;}
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+<table id="customers">
+    <tr>
+        <th>Nombre</th>
+        <th>Correo Electrónico</th>
+        <th>Archivos Vinculados</th>
+    </tr>
+    @foreach($users as $u)
+        <tr>
+            <td>{{$u->name}}</td>
+            <td>{{$u->email}}</td>
+            <td>-</td>
+        </tr>
+    @endforeach
+</table>
+
+</body>
+</html>
+
+{{--
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+--}}
+@endsection
